c13856810a52dd2c050b231162b27be7805a5454
Victor Tolbert <victor.tolbert@gmail.com>
2020-01-03 10:47:28 -0500

    update test button

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button id="test-button">Test Button</button>
+    <button id="test-button" @click="push">Test Button</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    push() {
+      // eslint-disable-next-line
+      dataLayer.push({
+        event: "vpv",
+        vpvUrl: "/virtual/createAccount/googleSignUpForm",
+        vpvTitle: "Registration Form – Google Signup Form"
+      });
+    }
+  }
+};
+</script>
