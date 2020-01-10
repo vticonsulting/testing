498d04f698b714b585a7ee3bc3fb2fd9cec79ba4
Victor Tolbert <victor.tolbert@gmail.com>
2020-01-10 15:37:56 -0500

    remove console.log statements

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,6 @@ export default {
       return (this.$route.meta.layout || default_layout) + "-layout";
     }
   },
-  created() {
-    // nothing defined here (when this.$route.path is other than "/")
-    console.log(this.$route, this.$route.meta.layout);
-  },
-  updated() {
-    // something defined here whatever the this.$route.path
-    console.log(this.$route, this.$route.meta.layout);
-  },
   mounted() {
     // const link = document.createElement("link");
     // link.setAttribute("rel", "stylesheet");
