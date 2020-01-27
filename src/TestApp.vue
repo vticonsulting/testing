035e9c19dcc7e6a2036f758b1a6fbd12089af0a1
Victor Tolbert <victor.tolbert@gmail.com>
2020-01-27 12:02:42 -0500

    IE11 troubleshooting...

--- src/TestApp.vue
+++ src/TestApp.vue
@@ -2,6 +2,7 @@
   <div id="app" class="h-screen w-full transition-all duration-300">
     <p>Begin IE 11 Troubleshooting</p>
     <p>Add plugins</p>
+    <p>plugins: buefy, date-fns</p>
   </div>
 </template>
 
